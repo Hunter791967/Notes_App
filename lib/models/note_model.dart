c71ab622ea3c04e83acaae0d0fabeca4b40cb4e5
Amr Elnahas <amr.elnahas2005@gmail.com>
2025-03-27 21:47:17 +0200

    Init Hive, create Note Box and make Constants File

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,6 +1,6 @@
 import 'package:hive/hive.dart';
 
-part 'note_model.g.dart';
+part 'note_model.g.dart'; //This file must be the same as model file name.
 @HiveType(typeId: 0)
 
 class NoteModel extends HiveObject {
