07ddeef46c3b4616bdb547c196b80792a4570b84
Amr Elnahas <amr.elnahas2005@gmail.com>
2025-03-27 19:27:34 +0200

    Install & ADD Hive, Hive_Flutter, Hive_Generator and Build Runner

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,24 +1,29 @@
 class NoteModel {
-  final int id;
+  // final int id;
   final String title;
   final String content;
+  final String date;
+  final int color;
 
   NoteModel({
-    required this.id,
+    required this.date,
+    required this.color,
     required this.title,
     required this.content,
   });
 
-  factory NoteModel.fromJson(Map<String, dynamic> json) {
-    final attributes =
-        json.containsKey('attributes') ? json['attributes'] : json;
-
-    //print("🔍 Extracted attributes: $attributes"); // Debug extracted attributes
-
-    return NoteModel(
-      id: json['id'] ?? 0, // Default to 0 if id is missing
-      title: attributes['title'] ?? 'No Title',
-      content: attributes['content']?.toString() ?? '',
-    );
-  }
+  // factory NoteModel.fromJson(Map<String, dynamic> json) {
+  //   final attributes =
+  //       json.containsKey('attributes') ? json['attributes'] : json;
+  //
+  //   //print("🔍 Extracted attributes: $attributes"); // Debug extracted attributes
+  //
+  //   return NoteModel(
+  //     //id: json['id'] ?? 0, // Default to 0 if id is missing
+  //     title: attributes['title'] ?? 'No Title',
+  //     content: attributes['content'] ?? '',
+  //     date: attributes['date'],
+  //     color: color,
+  //   );
+  // }
 }
