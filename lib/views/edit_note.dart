06164b3b21bf98b2180e4bbca10abd52fdefae55
Amr Elnahas <amr.elnahas2005@gmail.com>
2025-04-13 15:14:08 +0200

    Finish Edit Note

--- lib/views/edit_note.dart
+++ lib/views/edit_note.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
+import 'package:notes_app/cubits/showNotes/show_notes_cubit.dart';
 import 'package:notes_app/customWidgets/custom_app_bar.dart';
 import 'package:notes_app/models/note_model.dart';
 
 import '../customWidgets/custom_persistent_footer_button.dart';
+import '../customWidgets/custom_snack_bar_text.dart';
 import '../customWidgets/custom_text_field.dart';
 import '../utils/components/app_colors.dart';
 
-class EditNote extends StatelessWidget {
+class EditNote extends StatefulWidget {
   const EditNote({super.key, required this.noteModel});
 
   final NoteModel noteModel;
+
+  @override
+  State<EditNote> createState() => _EditNoteState();
+}
+
+class _EditNoteState extends State<EditNote> {
+  String? title, content;
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +44,25 @@ class EditNote extends StatelessWidget {
                 Icons.check_circle,
                 size: 40,
               ),
-              onPressed: () {},
+              onPressed: () {
+                //use widget before noteModel because of the state is stateful.
+                //change fields 'title, content' in note_model to be not final.
+                widget.noteModel.title = title ?? widget.noteModel.title;
+                widget.noteModel.content = content ?? widget.noteModel.content;
+                //Save the Updated NoteModel by using save() method in HiveObject.
+                widget.noteModel.save();
+                CustomSnackBarText(
+                  snackBarText: "Note Updated Successfully!",
+                  backgroundColor: AppColors.antiFlashWhite,
+                  snackBarColor: AppColors.dMedGreen,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  duration: 2,
+                ).showCustomSnackBar(context);
+                //Refresh Show Notes After Update.
+                BlocProvider.of<ShowNotesCubit>(context).fetchNotes();
+                Navigator.pop(context);
+              },
             ),
           ),
         ],
@@ -45,17 +73,21 @@ class EditNote extends StatelessWidget {
           child: Column(
             children: [
               CustomTextField(
+                initialValue: widget.noteModel.title,
                 hintText: 'Title',
                 colorOne: AppColors.antiFlashWhite,
                 colorTwo: AppColors.antiFlashWhite,
                 borderRadiusOne: 16,
                 borderRadiusTwo: 16,
                 fontSize: 13,
+                onChanged: (value) {
+                  title = value;
+                },
                 //textController: productNameController,
               ),
               const Gap(20),
               CustomTextField(
-                //textController: productPriceController,
+                initialValue: widget.noteModel.content,
                 hintText: 'Content',
                 //inputType: TextInputType.number,
                 colorOne: AppColors.antiFlashWhite,
@@ -64,6 +96,9 @@ class EditNote extends StatelessWidget {
                 borderRadiusTwo: 16,
                 fontSize: 13,
                 maxLines: 6,
+                onChanged: (value) {
+                  content = value;
+                },
               ),
               const Gap(20),
               const CustomPersistentFooterButton(
